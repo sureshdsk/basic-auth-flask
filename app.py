5777e0383a40fb74d1b6f65d3d5deeae3fb3f16c
DSK <sureshdsk91@gmail.com>
2015-09-26 17:58:48 +0530

    init awesome

---
+++ app.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from flask import Flask, request, Response
+app = Flask(__name__)
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/private")
+@requires_auth  #requires_auth decorator for basic auth
+def private_page():
+    return "Hello I'm Private!!"
+
+
+if __name__ == "__main__":
+    app.run()
